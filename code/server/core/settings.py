099b38f8fa7f3b9ca16c434890f2df8d2aea778c
sub0ne1 <iamsubhajit.d@gmail.com>
2023-11-05 17:44:56 -0500

    Auth: WIP

--- code/server/core/settings.py
+++ code/server/core/settings.py
@@ -121,7 +121,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
-    "DEFAULT_AUTHENTICATION_CLASSES": [
-        "rest_framework.authentication.TokenAuthentication",
-    ]
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
 }
