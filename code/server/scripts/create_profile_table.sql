3929d21e14607c66c0e0b92a39a1a827d68eff6d
YinXian-Cheng <yxczy123@gmail.com>
2023-11-10 16:25:17 -0500

    delete user/profile table sql

--- code/server/scripts/create_profile_table.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE profile (
-	id VARCHAR(50) PRIMARY KEY REFERENCES "user"(id),
-	name VARCHAR(20),
-	uni_name VARCHAR(30),
-	email VARCHAR(50)
-);
--- code/server/scripts/create_user_table.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE "user" (
-	id VARCHAR(50) PRIMARY KEY,
-	email VARCHAR(50),
-	phone_number VARCHAR(20),
-	verified BOOLEAN,
-	password VARCHAR(20),
-	last_login_at TIME,
-	created_at TIME
-);
