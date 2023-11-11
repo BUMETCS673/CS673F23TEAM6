193faa875cb8106d79268a24e5e90aac9599c301
AISHWARYA RAJA <65251355+aishuraja@users.noreply.github.com>
2023-11-10 22:15:45 -0500

    user info api : Done

--- code/server/account/urls.py
+++ code/server/account/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from rest_framework_simplejwt import views as jwt_views
 
 from .auth.api import RegisterAccountView
-from .views import CustomTokenObtainPairView
+from .views import CustomTokenObtainPairView, AccountView
 
 
 urlpatterns = [
@@ -11,4 +11,9 @@ urlpatterns = [
     path("register", RegisterAccountView.as_view()),
     path("login", CustomTokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("login/refresh", jwt_views.TokenRefreshView.as_view(), name="token_refresh"),
+
+    # Account URL
+    path("user", AccountView.as_view()),
     ]
+
+
