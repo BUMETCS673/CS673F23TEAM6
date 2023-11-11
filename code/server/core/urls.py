e63928c2d5fd013beee0685709d1cc92755d6d70
Jeet <97605913+0verread@users.noreply.github.com>
2023-11-11 00:46:16 -0500

    create product api (#48)

--- code/server/core/urls.py
+++ code/server/core/urls.py
@@ -4,6 +4,7 @@ from rest_framework_simplejwt import views as jwt_views
 
 urlpatterns = [
     path('api/users/', include('account.urls')),
+    path('api/', include('products.urls')),
     path('api/users/token', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/users/token/refresh', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
 ]
