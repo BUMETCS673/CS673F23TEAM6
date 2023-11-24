0c4ef29c27ab6adb590d013073e60c31ede67568
sub0ne1 <iamsubhajit.d@gmail.com>
2023-11-24 14:32:58 -0500

    Add Logout API: :check:

--- code/server/account/urls.py
+++ code/server/account/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from rest_framework_simplejwt import views as jwt_views
 
 from .auth.api import RegisterAccountView
-from .views import CustomTokenObtainPairView, AccountView
+from .views import CustomTokenObtainPairView, AccountView, LogoutView
 
 
 urlpatterns = [
@@ -11,6 +11,7 @@ urlpatterns = [
     path("register", RegisterAccountView.as_view()),
     path("login", CustomTokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("login/refresh", jwt_views.TokenRefreshView.as_view(), name="token_refresh"),
+    path("logout", LogoutView.as_view(), name="auth_logout")
 
     # Account URL
     path("user", AccountView.as_view()),
