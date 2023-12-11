280a3be251af17e6511ba2ed9b6997edbc5ea33a
Jeet <97605913+0verread@users.noreply.github.com>
2023-12-11 11:21:49 -0500

    Logout  and Change password API  (#90)

--- code/server/account/auth/api.py
+++ code/server/account/auth/api.py
@@ -1,5 +1,6 @@
 from rest_framework.views import APIView
-from rest_framework import status
+from rest_framework import status, generics
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
 from account.models import CustomUser
--- code/server/account/urls.py
+++ code/server/account/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from rest_framework_simplejwt import views as jwt_views
 
+from .views import CustomTokenObtainPairView, AccountView, LogoutView
 from .auth.api import RegisterAccountView, ChangePasswordView
 from .views import CustomTokenObtainPairView, AccountView
 
@@ -11,9 +12,10 @@ urlpatterns = [
     path("register", RegisterAccountView.as_view()),
     path("login", CustomTokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("login/refresh", jwt_views.TokenRefreshView.as_view(), name="token_refresh"),
-    path("/changepass", ChangePasswordView.as_view(), name="change_password")
+    path("logout", LogoutView.as_view(), name="auth_logout"),
+    path("changepass", ChangePasswordView.as_view(), name="change_password"),
 
-    # Account URL
+    # Account Info URL
     path("user", AccountView.as_view()),
     ]
 
