88812d6442ef70877d916b595c81edabaa4c465b
sub0ne1 <iamsubhajit.d@gmail.com>
2023-12-11 10:56:41 -0500

    product apis

--- code/server/products/urls.py
+++ code/server/products/urls.py
@@ -2,11 +2,15 @@ from django.urls import path
 
 from rest_framework_simplejwt import views as jwt_views
 
-from .views import ProductPostAPIView
+from .views import ProductPostAPIView, GetAllProductsAPIView, GetProductByIdAPIView, UpdateProductByIdAPIView, DeleteProductByIdAPIView
 
 
 urlpatterns = [
-    path("products/create", ProductPostAPIView.as_view()),
+    path('products/create', ProductPostAPIView.as_view()),
+    path('/products', GetAllProductsAPIView.as_view()),
+    path('/products/<int:pd_id>', GetProductByIdAPIView.as_view()),
+    path('/products/update/<int:pd_id>', UpdateProductByIdAPIView.as_view()),
+    path('/products/delete/<int:pd_id>', DeleteProductByIdAPIView.as_view())
     ]
 
 
